c6b0d22fb96b384d2997aa1fdc83b974bf154077
washington <washington.zora@gmail.com>
2019-09-27 23:15:40 -0300

    Inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public lottieConfig;
+  public lottieConfig2;
+  public lottieConfig3;
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
 
-  constructor() {}
-
+  constructor() {
+    this.lottieConfig = {
+      path: 'https://assets4.lottiefiles.com/packages/lf20_hu9cd9.json'
+    };
+    this.lottieConfig2 = {
+      path:
+        'https://assets4.lottiefiles.com/datafiles/Hc0DflKIkYg1j3u/data.json'
+    };
+    this.lottieConfig3 = {
+      path: 'https://assets9.lottiefiles.com/packages/lf20_kQqOcL.json'
+    };
+  }
 }
