50960179cdc1ef45e765b3380aa3b537fbb2e24f
washington <washingtonzora@gmail.com>
2019-09-29 19:26:00 -0300

    Configurações finais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,10 +9,8 @@ export class HomePage {
   public lottieConfig;
   public lottieConfig2;
   public lottieConfig3;
-  slideOpts = {
-    initialSlide: 1,
-    speed: 400
-  };
+  public lottieConfig4;
+  public lottieConfig5;
 
   constructor() {
     this.lottieConfig = {
@@ -24,6 +22,14 @@ export class HomePage {
     };
     this.lottieConfig3 = {
       path: 'https://assets9.lottiefiles.com/packages/lf20_kQqOcL.json'
+    };
+    this.lottieConfig4 = {
+      path: 'https://assets4.lottiefiles.com/packages/lf20_dyXaL5.json'
+    };
+
+    this.lottieConfig5 = {
+      path:
+        'https://api.lottiefiles.com/v2/force-download-file?url=https://assets7.lottiefiles.com/temp_editor_files/lf30_editor_dAdWn1.json'
     };
   }
 }
